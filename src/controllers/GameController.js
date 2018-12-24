0c4ce0d7e0f8d8bf316f28b10fc31231183afe70
David Vazquez <40529534+dmvazli@users.noreply.github.com>
2018-12-24 16:23:44 -0500

    added resetGame method

--- src/controllers/GameController.js
+++ src/controllers/GameController.js
@@ -1,8 +1,32 @@
 /* *************************************************************************************************** */
+/*                       IMPORTS                                                                       */
+/* *************************************************************************************************** */
+
+import $ from 'jquery';
+import 'jquery-ui/ui/widgets/draggable';
+
+import Board from '../models/Board';
+import Position from '../models/Position.js';
+import Coin from '../models/Coin.js';
+
+import * as positionView from '../views/positionView.js';
+import * as coinView from '../views/coinView.js';
+import * as timerView from '../views/timerView.js';
+import * as movesView from '../views/movesView.js';
+import * as attemptsView from '../views/attemptsView.js'
+import * as hintButtonView from '../views/hintButtonView';
+
+import {elements} from '../views/base.js';
+
+import {gameState} from '../index.js';
+
+import * as cookie from './cookieController.js'
+
+/* *************************************************************************************************** */
 /*                       GAME CONTROLLER                                                               */
 /* *************************************************************************************************** */
 
-export const board = new Board();
+export let board = new Board();
 
 const INITIAL_BOARD_STATE = $(elements.board).clone();
 
@@ -48,12 +72,14 @@ export default class GameController
     initializeTimer()
     {   
         cookie.destroy();
+
+        //console.log('COOKIE:', cookie);
         
         let minutes = cookie.get('minutes').length === 0 ? 0 : parseInt(cookie.get('minutes'));
         let seconds = cookie.get('seconds').length === 0 ? 0 : parseInt(cookie.get('seconds'));
         let totalSeconds = cookie.get('totalSeconds').length === 0 ? 0 : parseInt(cookie.get('totalSeconds'));
 
-        console.log('Minutes:', minutes, 'Seconds:', cookie.get('seconds'));
+        //console.log('Minutes:', minutes, 'Seconds:', seconds);
         
         cookie.set('totalSeconds', (minutes * 60) + seconds);
 
@@ -69,14 +95,15 @@ export default class GameController
 
     solutionFound()
     {
-        console.log('GAME WON??');
         return board.solutionFound();
     }
 
     resetBoard()
     {   
+        board = new Board();
+        
         movesView.resetMoves();
-        movesView.incrementAttempts(gameState.attempts);
+        attemptsView.incrementAttempts(gameState.attempts);
         
         hintButtonView.resetHintButton();
         
@@ -92,32 +119,34 @@ export default class GameController
 
         gameState.reset();
     }
+
+    resetGame()
+    {
+        board = new Board();
+
+        attemptsView.resetAttempts();
+        movesView.resetMoves();
+
+        timerView.resetTime();
+        timerView.stopTimer();
+
+        hintButtonView.resetHintButton();
+
+        gameState.resetAll();
+
+        $(elements.board).replaceWith(INITIAL_BOARD_STATE.clone());
+
+        elements.refresh();
+
+        positionView.bindDroppable(elements.allPositions);
+        coinView.bindDraggable(elements.allCoins);
+
+        this.initializePositions();
+        this.initializeCoins();
+    }
 }
 
 function parseID(id)
 {
     return parseInt(id.match(/\d+/)[0]);
 }
-
-/* *************************************************************************************************** */
-/*                       IMPORTS                                                                       */
-/* *************************************************************************************************** */
-
-import $ from 'jquery';
-import 'jquery-ui/ui/widgets/draggable';
-
-import Board from '../models/Board';
-import Position from '../models/Position.js';
-import Coin from '../models/Coin.js';
-
-import * as positionView from '../views/positionView.js';
-import * as coinView from '../views/coinView.js';
-import * as timerView from '../views/timerView.js';
-import * as movesView from '../views/movesView.js';
-import * as hintButtonView from '../views/hintButtonView';
-
-import {elements} from '../views/base.js';
-
-import {gameState} from '../index-test.js';
-
-import * as cookie from './cookieController.js'
