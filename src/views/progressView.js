9534f6868b04fcb1586c43d779bb3f092d38af69
David Vazquez <40529534+dmvazli@users.noreply.github.com>
2019-01-26 21:03:59 -0500

    converted to ES6 class

--- src/views/progressView.js
+++ src/views/progressView.js
@@ -1,56 +1,92 @@
-/* *************************************************************************************************** */
-/*                                   PROGRESS VIEW                                                     */
-/* *************************************************************************************************** */
-
-/* *************************************************************************************************** */
-/*                                      IMPORTS                                                        */
-/* *************************************************************************************************** */
+console.log('progressView.js');
 
 import $ from 'jquery';
-import {gameState} from '../index.js';
+import {game} from '../index.js';
 import {elements} from '../views/base.js';
 
 /* *************************************************************************************************** */
-/*                                  EVENT LISETNERS                                                    */
+/*                                        PROGRESS VIEW                                                */
+/* *************************************************************************************************** */
+
+export default class ProgressMessageView
+{
+    constructor()
+    {
+        this.numMoves = null;
+        this.minutes = null;
+        this.seconds = null;
+        this.attempts = null;
+        this.hintsUsed = null;
+    }
+    
+    displayProgressMessage()
+    {   
+        this.initializeGameStats();
+    
+        this.numMoves === 3 ? this.initializeProgressMessage(true) :
+                                 this.initializeProgressMessage(false);
+
+        this.updateContinueButton();
+    
+        this.blurGameBoard();
+        this.fadeInProgressMessage();
+    }
+
+    blurGameBoard()
+    {
+        elements.board.style.filter = 'blur(10px)';
+        elements.board.style.transition = '2s filter linear';
+    }
+
+    initializeGameStats()
+    {
+        this.numMoves = game.moves;
+        this.minutes = game.currentTime.minutes;
+        this.seconds = game.currentTime.seconds;
+        this.attempts = game.attempts;
+        this.hintsUsed = game.hintsUsed;
+
+        elements.stat1.innerText = this.attempts;
+        elements.stat2.innerText = `${this.minutes > 0 ? this.minutes + ' min,' : ''} ${this.seconds} sec`;
+        elements.stat3.innerText = `${this.hintsUsed ? 'Yes' : 'No'}`;
+    }
+
+    initializeProgressMessage(solvedInThree)
+    {
+        if(solvedInThree) {
+            elements.progressMessage1.innerText = 'Congratulations! You solved the puzzle in 3 moves!';
+            elements.progressMessage2.innerText = 'Try again in a few days and see how quickly you can solve it again!';
+        }
+        else {
+            elements.progressMessage1.innerText = `You solved the puzzle in ${this.numMoves} moves.`;
+            elements.progressMessage2.innerText = 'But can you solve it in 3...';
+        }
+    }
+
+    updateContinueButton()
+    {
+        if(game.solvedInThree)
+            $(elements.progressContinueButton).val('Restart Puzzle');
+    }
+
+    fadeInProgressMessage()
+    {
+        $(elements.progressMessageContainer).fadeIn(2000);
+        $('#game-container *').prop('disabled', true);
+        $('.coin').css({pointerEvents: 'none'});
+    }
+}
+
+/* *************************************************************************************************** */
+/*                                        EVENT LISTENER                                               */
 /* *************************************************************************************************** */
 
 elements.progressContinueButton.addEventListener('click', () =>
-{
-    $(elements.progressMessageContainer).finish().fadeOut(10);
-    elements.board.style.filter = 'blur(0px)';
-    
+{        
+    if(game.solvedInThree)
+        $(elements.progressMessageContainer).fadeOut(500);
+
     document.dispatchEvent(new Event('next-attempt-started'));
-})
+});
 
-/* *************************************************************************************************** */
-/*                                       FUNCTIONS                                                     */
-/* *************************************************************************************************** */
 
-export const displayProgress = () =>
-{
-    let numMoves = gameState.moves;
-    let minutes = gameState.currentTime.minutes;
-    let seconds = gameState.currentTime.seconds;
-    let attempts = gameState.attempts;
-    let hintsUsed = gameState.hintsUsed;
-
-    elements.board.style.filter = 'blur(10px)';
-    elements.board.style.transition = '1s filter linear';
-
-    if(gameState.moves > 3)
-    {
-        elements.progressMessage1.innerText = `You solved the puzzle in ${numMoves} moves.`;
-        elements.progressMessage2.innerText = 'But can you solve it in 3...';
-    }
-    else
-    {
-        elements.progressMessage1.innerText = 'Congratulations! You solved the puzzle in 3 moves!';
-        elements.progressMessage2.innerText = 'Try again in a few days and see how quickly you can solve it again!';
-    }
-
-    elements.stat1.innerText = attempts;
-    elements.stat2.innerText = `${minutes > 0 ? minutes + ' min,' : ''} ${seconds} sec`;
-    elements.stat3.innerText = `${hintsUsed ? 'Yes' : 'No'}`;
-
-    $(elements.progressMessageContainer).fadeIn(2000);
-}
