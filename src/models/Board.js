d3ed7689743943f5b79947f8cfbae52daf8ebba2
David Vazquez <40529534+dmvazli@users.noreply.github.com>
2019-01-26 20:50:46 -0500

    updated board model
    this file needs some work/comments

--- src/models/Board.js
+++ src/models/Board.js
@@ -1,49 +1,75 @@
+console.log('board.js');
+
+import Coin from "./Coin";
+import Position from "./Position";
+
 /* *************************************************************************************************** */
-/*                       GAME BOARD MODEL                                                              */
+/*                                         BOARD MODEL                                                 */
 /* *************************************************************************************************** */
 
 const ADJACENT_OFFSETS  = [[-1, -1], [ 0, -1],      // top-left, top-right
                            [ 1,  0], [ 1,  1],      // right, bottom-right
                            [ 0,  1], [-1,  0]];     // bottom-left, left
 
-const WINNING_OFFSETS =  /* top-left */ [[1, 0],    // top-right   
+const WINNING_OFFSETS =                 [[1, 0],     
                                  [2, 1], [2, 2],    
                                  [1, 2], [0, 1]];
 
-const BLOCKING_PATTERNS = [[true , false, true , false, true , false],   // 3-coin block
-                           [false, true , false, true , false, true ],   // 3-coin block
-                           [true , true , false, true , true , false],   // 4-coin block
-                           [false, true , true , false, true , true ],   // 4-coin block
-                           [true , false, true , true , false, true ]];  // 4-coin block
-
-export default class Board
+export default class Board 
 {
     constructor()
     {
         this.board;
+        this.occupiedPosIDs = [6, 7, 8, 11, 12, 13];
+        this.openPositions;
+        this.surroundingCoins;
+
         this.init();
     }
 
     init()
     {
-        this.board = Array(5);          // x, columns
+       this.initializeBoard();
+       this.initializeOccupiedPostions();
+    }
+
+    initializeBoard()
+    {
+        this.board = Array(5);          
 
         for(let x = 0; x < 5; x++)
-            this.board[x] = Array(4);   // y, rows
+            this.board[x] = Array(4);   
+    }
+
+    initializeOccupiedPostions()
+    {
+        let coinID = -1;
+        
+        for(let i = 0; i < 20; i++)
+        {
+            this.add(new Position(i));
+
+            if(this.occupiedPosIDs.includes(i))
+                this.addCoinTo(i, ++coinID);
+        }
     }
 
     add(newPosition)
     {
         var {x, y} = get2DIndex(newPosition.id);
-
+        
         this.board[x][y] = newPosition;
     }
 
-    addCoinTo(posID, newCoin)
+    addCoinTo(posID, coinID)
     {
         var {x, y} = get2DIndex(posID);
 
-        this.board[x][y].addCoin(newCoin);
+        this.board[x][y].addCoin(new Coin(coinID));
+
+        this.addOccupiedPosID(posID);
+
+        console.log('ADDED OCC-POSITION:', posID, 'TO:', this.occupiedPosIDs);
     }
 
     removeCoinFrom(posID)
@@ -51,68 +77,100 @@ export default class Board
         var {x, y} = get2DIndex(posID);
 
         var removedCoin = this.board[x][y].removeCoin();
+        
+        this.removeOccupiedPosID(posID);
+        this.initializeOpenPositions();
+
+        console.log('REMOVED OCC-POSITION:', posID, 'FROM:', this.occupiedPosIDs);
 
         return removedCoin;
     }
 
-    getCoinsBlocking(posID) // !!!
-    {   
-        let blockingCoins = [];
-        const testPattern = [];
+    removeOccupiedPosID(posID)
+    {
+        var index = this.occupiedPosIDs.indexOf(posID);
         
-        for(let {adjX, adjY} of getAdjacentCoordinates(posID))
-        {
-            if(inBounds(adjX, adjY))
-            {
-                var adjacentPosition = this.board[adjX][adjY];
-
-                if(adjacentPosition.hasCoin)
-                {
-                    blockingCoins.push(adjacentPosition.getCoinID());
-                    testPattern.push(true);
-                }
-                else
-                    testPattern.push(false);
-            }
-            else
-                testPattern.push(false);
-        };
-
-        for(let casePattern of BLOCKING_PATTERNS)
-        {
-            var coinsAreBlocking = true;
-
-            for(let i = 0; i < casePattern.length && coinsAreBlocking; i++)
-                if(casePattern[i] === true && testPattern[i] === false)
-                    coinsAreBlocking = false;
-            
-            if(coinsAreBlocking)
-                return blockingCoins;
-        }
-
-        return [];
+        this.occupiedPosIDs.splice(index, 1);
     }
 
-    getOpenPositions() // !!!
+    addOccupiedPosID(posID)
     {
-        let adjacentCoins = 0;
+        if(!this.occupiedPosIDs.includes(posID))
+            this.occupiedPosIDs.push(posID);
+    }
+
+    initializeOpenPositions()
+    {
+        let adjacentCoins;
         const openPositions = [];
 
+        // iterate through every position on the board
         for(let currentPosition of board_toArray(this.board))
         { 
-            if(!currentPosition.hasCoin /* && this.getCoinsBlocking(currentPosition.id).length < 0 */) //second condition currentPosition.coin.isBlocking?
-            {
-                for(let {adjX, adjY} of getAdjacentCoordinates(currentPosition.id))
-                    if(inBounds(adjX, adjY) && this.board[adjX][adjY].hasCoin)
-                        adjacentCoins++;
-
-                if(adjacentCoins >= 2) openPositions.push(currentPosition.id);
-            }
-
             adjacentCoins = 0;
+            
+            // if the current position does not have a coin
+            if(!currentPosition.hasCoin /* && !this.positionBlocked(currentPosition) */)
+            {
+                // iterate through the coordinates of the positions surrounding the current position
+                for(let {adjX, adjY} of getAdjacentCoordinates(currentPosition.id))
+                {
+                    // if the coordinates are in bounds of the board, proceed
+                    if(inBounds(adjX, adjY))
+                    {
+                        var adjacentPosition = this.board[adjX][adjY];
+
+                        // if the adjacent position has a coin, increment adjacentCoints
+                        if(adjacentPosition.hasCoin)
+                            adjacentCoins++;
+                    }
+                }
+
+                // if there are 2 or more adjacent coins, the position is available to be occupied
+                if(adjacentCoins >= 2) 
+                    openPositions.push(currentPosition.id);
+            }
         }
 
-        return openPositions;
+        this.openPositions = openPositions;
+    }
+
+    coinIsBlocked(coinPosID)
+    {   
+        let consecutiveOpenPositions = 0;
+        let surroundingCoins = [];
+
+        // iterate through the coordinates of all surrounding positions
+        for(let {adjX, adjY} of getAdjacentCoordinates(coinPosID, true))
+        {
+            // if the coordinates are out of bounds, the coin is not blocked
+            if(!inBounds(adjX, adjY))
+                return false; // return an empty array
+            
+            // get the position from the board using the adjancent coordinates
+            var adjacentPosition = this.board[adjX][adjY];
+
+            // if the adjacent position does not have a coin
+            if(!adjacentPosition.hasCoin)
+                // increment consecutiveOpenPositions
+                consecutiveOpenPositions++;
+            else 
+            {
+                // reset consecutiveOpenPositions to '0'
+                consecutiveOpenPositions = 0;
+                // and push the 'id' of the adjacent coin to the blockingCoinsarray
+                surroundingCoins.push(adjacentPosition.getCoinID());
+            }
+            
+            // if two consecutive open positions are found, the coin is not blocked
+            if(consecutiveOpenPositions === 2)
+                return false; // return an empty array
+        }
+
+        this.surroundingCoins = surroundingCoins.slice(0, 5);
+
+        // id's of all blocking coins will return if coin is not blocked
+        return true;
     }
 
     solutionFound()
@@ -188,7 +246,7 @@ function inBounds(x, y)
     return x >= 0 && x <= 4 && y >= 0 && y <= 3;
 }
 
-function getAdjacentCoordinates(posID)
+function getAdjacentCoordinates(posID, extendToFirstCoordinate = false)
 {
     var {x, y} = get2DIndex(posID);
 
@@ -201,6 +259,9 @@ function getAdjacentCoordinates(posID)
 
         adjacentCoordinates.push({adjX, adjY})
     }
+
+    if(extendToFirstCoordinate)
+        adjacentCoordinates.push(adjacentCoordinates[0]);
 
     return adjacentCoordinates;
 }
@@ -221,9 +282,3 @@ function getWinningCoordinates(posID)
 
     return winningCoordinates;
 }
-
-// function parse1DIndex(x, y)
-// {
-//     return (y * 5) + x;
-// }
-
