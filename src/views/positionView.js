a2cddac5c689b543f25135f446a15ea7417c1d61
David Vazquez <40529534+dmvazli@users.noreply.github.com>
2019-01-26 21:03:30 -0500

    converted to ES6 class

--- src/views/positionView.js
+++ src/views/positionView.js
@@ -1,74 +1,143 @@
-/* *************************************************************************************************** */
-/*                                   POSITION VIEW                                                     */
-/* *************************************************************************************************** */
-
-/* *************************************************************************************************** */
-/*                                      IMPORTS                                                        */
-/* *************************************************************************************************** */
+console.log('positionView.js')
 
 import $ from 'jquery';
 import 'jquery-ui/ui/widgets/droppable';
 import 'jquery-shadow-animation/jquery.animate-shadow';
 import {elements} from './base.js';
+import {game} from '../index.js';
 
 /* *************************************************************************************************** */
-/*                       EVENT LISTENERS                                                               */
+/*                                        POSITION VIEW                                                */
 /* *************************************************************************************************** */
 
-export const bindDroppable = position =>
+export default class PositionView
 {
-    $(position).droppable(
+    constructor()
     {
-        over: function()
-        {
-            document.dispatchEvent(new CustomEvent('coin-over',
-            {   
-                detail: {posID: this.id}
-            }));
-        },
+        $(elements.allPositions).droppable({
+            disabled: true
+        })
 
-        drop: function()
-        {
-            document.dispatchEvent(new CustomEvent('coin-dropped',
-            {   
-                detail: {posID: this.id}
-            }));
-        }
-    });
-}
-bindDroppable(elements.classPosition);
-
-/* *************************************************************************************************** */
-/*                              FUNCTIONS                                                              */
-/* *************************************************************************************************** */
-
-export const dropEnabled = (toggle, posID) =>
-{
-    var position = elements.position(posID);
-
-    $(position).droppable('option', 'disabled', !toggle);
-}
-
-export const positionDroppable = (toggle, posID) =>
-{
-    var position = elements.position(posID);
-
-    $(position).droppable('option', 'disabled', !toggle);
-}
-
-export const positionBeveled = (toggle, posID) =>          
-{
-    var position = elements.position(posID);
-
-    if(toggle)
-    {
-        $(position).stop().animate({boxShadow: 'inset 4px 4px 8px rgba(0, 0, 0, 0.33)', backgroundColor: '#c2c2c2'}, 100);
-        position.style['border'] = '1px solid #666666';
+        this.bindPositionEvents(elements.allPositions);
     }
-    else
+
+    bindPositionEvents(position)
     {
-        $(position).stop().animate({boxShadow: 'inset 0px 0px 0px rgba(0, 0, 0, 0.33)', backgroundColor: '#d1d1d1'}, 100);
-        position.style['border'] = '1px solid #919191';
+        $(position).on('dropover', function() {    
+            game.moveCoinOver(this.id);
+        });
+        
+        $(position).on('drop', function() {
+            game.dropCoin(this.id);
+        });
+    }
+
+    disableDrop(positionIDs)
+    {        
+        if(typeof positionIDs === 'number')
+        {
+            let position = elements.position(positionIDs);
+
+            $(position).droppable('option', 'disabled', true);
+        }    
+        else
+        {
+            positionIDs.forEach(posID =>
+            {
+                let position = elements.position(posID);
+
+                $(position).droppable('option', 'disabled', false);
+            });
+        }
+    }
+
+    enableDrop(positionIDs)
+    {
+        positionIDs.forEach(posID =>
+        {
+            let position = elements.position(posID);
+    
+            $(position).droppable('option', 'disabled', false);
+        });
+    }
+
+    disableDrop(positionIDs)
+    {
+        if(typeof positionIDs === 'number')
+        {
+            let position = elements.position(positionIDs);
+    
+            $(position).droppable('option', 'disabled', true);
+        }    
+        else
+        {
+            positionIDs.forEach(posID =>
+            {
+                let position = elements.position(posID);
+    
+                $(position).droppable('option', 'disabled', true);
+            });
+        }
+    }
+
+    revealOpenPositions(positions)
+    {
+        positions.forEach(posID =>
+        {
+            let position = elements.position(posID);
+    
+            this.bevelPosition(position);
+        });
+    }
+
+    concealOpenPositions(positions)
+    {
+        positions.forEach(posID =>
+        {
+            let position = elements.position(posID);
+    
+            this.unbevelPosition(position);
+        });
+    }
+
+    bevelPosition(position)
+    {
+        $(position).finish().animate(
+        {
+            boxShadow: 'inset 4px 4px 8px rgba(0, 0, 0, 0.33)', 
+            backgroundColor: '#c2c2c2'
+        }, 
+        {
+            duration: 100, 
+            queue: false
+        });
+        
+        $(position).css({border: '1px solid #666666'});
+    }
+
+    unbevelPosition(position)
+    {
+        $(position).finish().animate(
+        {
+            boxShadow: 'inset 0px 0px 0px rgba(0, 0, 0, 0.33)', 
+            backgroundColor: '#d1d1d1'
+        }, 
+        {
+            duration: 100, 
+            queue: false
+        });
+    
+        $(position).css({border: '1px solid #a7a6a6'});
+    }
+
+    removeCoinsFrom(occupiedPositions)
+    {
+        console.log('OCCUPIED POSITIONS: ', occupiedPositions);
+        
+        occupiedPositions.forEach(posID =>
+        {
+            $(elements.position(posID)).children().remove();
+        });
     }
 }
 
